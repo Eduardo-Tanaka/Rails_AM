cf53461e15c03e285b943e20c73124802cf8ba50
Eduardo <eduardo.s.tanaka@gmail.com>
2015-07-30 11:19:04 -0300

    host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,16 +41,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    #:user_name      => ENV['SENDGRID_USERNAME'],
-    #:password       => ENV['SENDGRID_PASSWORD'],
-    :user_name      => 'app39366419@heroku.com',
-    :password       => '34f4vhd58653',
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
 
 end
