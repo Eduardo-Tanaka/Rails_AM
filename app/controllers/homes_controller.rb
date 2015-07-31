cc2b5662c5ff9ffddd3538cfef6961f942dbb99b
Eduardo <eduardo.s.tanaka@gmail.com>
2015-07-31 11:54:44 -0300

    fix bug person

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,17 @@
 class HomesController < ApplicationController
+
+  before_action :find_user
+
   def index
     @donations = Donation.includes(:photos).all.order("created_at DESC").where(status: 1).limit(8)
   end
   def search
     @donations = Donation.includes(:photos).where(status: 1).where("title like ? or description like ?", "%#{params[:search]}%", "%#{params[:search]}%")
+  end
+
+  private
+
+  def find_user
+    @person = Person.find(current_user.id) if current_user != nil
   end
 end
